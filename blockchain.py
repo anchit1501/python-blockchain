5fd044e0a2832c7fcfec4f4088b54c7e662e1123
anchit1501 <1501.anchit@gmail.com>
2019-03-19 00:22:50 +0530

    Loops(while-for) and Conditionals Added (if-else)

--- blockchain.py
+++ blockchain.py
@@ -1,34 +1,67 @@
 # Initializing our blockchain list
-blockchain  = []
+blockchain = []
 
 
 def get_last_blockchain_value():
     """Returns the last value of current blockchain."""
+    # len inbuild fuction for checking length of a variable
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
 
-def add_value(transaction_amount,last_transaction_value=[1]):
+def add_transaction(transaction_amount, last_transaction_value=[1]):
     """Append a new value as well as last value to the blockchain
             Arguments:
             :transaction_amout: The amount that should be added
             :last_transaction_value: The last blockchain transaction (Deafult([1]))
     """
-    blockchain.append([last_transaction_value,transaction_amount])
+    if last_transaction_value == None:
+        last_transaction_value = [1]
+    blockchain.append([last_transaction_value, transaction_amount])
 
 
-def get_user_input():
-    """ Returns the inpput to thw user"""
+def get_transaction_value():
+    """ Returns the input to the user"""
     return(float(input('Your Transaction Amount Please: ')))
 
+def get_user_choice():
+    """ Returns the input to the user"""
+    return(input('Your Choice: '))
 
-  
-tx_amount= get_user_input()
-add_value(tx_amount)
+def print_blockchain_elements():
+    # Output the blockchain list to the console
+    # For Loop
+    for block in blockchain:
+        print('Outputting Block')
+        print(block)
 
-tx_amount= get_user_input()
-add_value(last_transaction_value=get_last_blockchain_value(),transaction_amount=tx_amount)
+# While Loop
+while True:
+    print('Please Choose')
+    print('1. Add new trnsaction value')
+    print('2. Output the blockchain blocks')
+    print('3. Quit')
+    user_choice=get_user_choice()
+    if user_choice == '1':
+        tx_amount=get_transaction_value()
+        add_transaction(tx_amount, get_last_blockchain_value())
 
-tx_amount= get_user_input()
-add_value(tx_amount,get_last_blockchain_value())
+    elif user_choice == '2':
+        print_blockchain_elements()
 
-print(blockchain)
+    elif user_choice == '3':
+        # Option 1
+        break
+        # Option 2
+        # Skips the code to restart the loop
+        # continue
+
+
+    else:
+        print('Invalid Input, Please pick value from list')
+    print('Choice Registered!!!!')
+       
+print('Done!')
+
+# print(blockchain)
